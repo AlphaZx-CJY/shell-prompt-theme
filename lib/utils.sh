2590af3331dadb8eee5e592f3eeb17b67bb09b1c
AlphaZx <congjiye@outlook.com>
2026-04-25 14:12:47 +0800

    style: soften path color to green and simplify user info

--- lib/utils.sh
+++ lib/utils.sh
@@ -13,7 +13,5 @@ __prompt_shorten_path() {
 }
 
 __prompt_user_host() {
-    local user="${USER:-$(whoami)}"
-    local host="${HOSTNAME:-$(hostname -s)}"
-    printf '%s@%s' "$user" "$host"
+    printf '%s' "${USER:-$(whoami)}"
 }
